5ff143b871a5f014a8720fd75abf618bf2b84f9f
Andreas Dahl <andreas@hrdahl.dk>
2015-09-03 19:48:18 +0200

    bbox and new curve definition

--- ass0.hs
+++ ass0.hs
@@ -17,25 +17,30 @@ instance Eq Point where
         abs(xa - xb) < 0.01 && abs(ya - yb) < 0.01
 
 
-data Curve = Curve Point [Point]
+data Curve = Curve [Point]
     deriving (Show)
 
 curve :: Point -> [Point] -> Curve
-curve p ps = Curve p ps
+curve p ps = Curve (p:ps)
 
 connect :: Curve -> Curve -> Curve
-connect (Curve a as) (Curve b bs) = curve a (as ++ (b : bs))
+connect (Curve as) (Curve bs) = Curve (as ++ bs)
 
 rotate :: Curve -> Double -> Curve
-rotate (Curve a as) r = Curve (fr' a) (map fr' as)
+rotate (Curve as) r = Curve (map fr' as)
     where
         fr lcos lsin (Point (x,y)) = (point (x*lcos-y*lsin, x*lsin+y*lcos))
         fr' = (fr (cos (r/180.0*pi)) (sin (r/180.0*pi)))
 
 translate :: Curve -> Point -> Curve
-translate (Curve a as) p = Curve (ft p a) (map (ft p) as)
+translate (Curve as) p = Curve (map (ft p) as)
     where
         ft (Point(xa, ya)) (Point(xb, yb)) = (Point(xa+xb,ya+yb))
+
+bbox :: Curve -> (Point, Point)
+bbox (Curve ps) = (Point (minimum (map pointX ps), minimum (map pointY ps)),
+                   Point (maximum (map pointX ps), maximum (map pointY ps)))
+        
 
 {-
 curve a ((as : b) ++ bs)
