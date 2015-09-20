6cf2ab918c1331fc90a5cba13876e4db3ef5d513
AndreasDahl <andreas@hrdahl.dk>
2015-09-20 17:55:26 +0200

    a little quickCheck

--- ass1/src/SalsaInterpTest.hs
+++ ass1/src/SalsaInterpTest.hs
@@ -5,7 +5,10 @@ module SalsaInterpTest where
 import Test.HUnit
 import Test.QuickCheck
 
+import SalsaAst
 import SalsaInterp
+
+-- Unit Tests
 
 test1 = TestCase
     (assertEqual
@@ -15,26 +18,84 @@ test1 = TestCase
 
 main = runTestTT (TestList [TestLabel "test1" test1])
 
+-- QuickCheck
+
 newtype SmallInts = SI Integer
     deriving (Eq, Show)
 
-newtype Pos = Pos Position
+newtype Posi = Pos Position
     deriving (Eq, Show)
 
 instance Arbitrary SmallInts where
     arbitrary = SI <$> choose (1, 60)
 
-instance Arbitrary Pos where
+instance Arbitrary Posi where
     arbitrary = do
         Positive x <- arbitrary
         Positive y <- arbitrary
         return $ Pos (x, y)
 
+instance Arbitrary Colour where
+    arbitrary = elements [Blue, Plum, Red, Green, Orange]
 
-prop_checkInterpolateLength (SI i) (Pos p1) (Pos p2) = length (interpolate i p1 p2) == fromIntegral i
-prop_checkInterpolateEndPoint (SI i) (Pos p1) (Pos p2) = last (interpolate i p1 p2) == p2
+instance Arbitrary Expr where
+    arbitrary = do
+        Small i <- arbitrary
+        return $ Const i
+
+absPosGen = do
+    x <- arbitrary
+    y <- arbitrary
+    return $ Abs x y
+
+relPosGen = do
+    x <- arbitrary
+    y <- arbitrary
+    return $ Rel x y
 
 
+instance Arbitrary Pos where
+    arbitrary = oneof [absPosGen, relPosGen]
+
+rectGen = do
+    i <- arbitrary
+    x <- arbitrary
+    y <- arbitrary
+    w <- arbitrary
+    h <- arbitrary
+    c <- arbitrary
+    b <- arbitrary
+    return $ Rect i x y w h c b
+
+circleGen =  do
+    i <- arbitrary
+    x <- arbitrary
+    y <- arbitrary
+    r <- arbitrary
+    c <- arbitrary
+    b <- arbitrary
+    return $ Circ i x y r c b
+
+moveGen = do
+    i <- arbitrary
+    p <- arbitrary
+    return $ Move i p
+
+toggleGen = do
+    i <- arbitrary
+    return $ Toggle i
+
+instance Arbitrary Command where
+    arbitrary = oneof [rectGen, circleGen]
+
+
+-- Interpolate
+prop_InterpolateLength (SI i) (Pos p1) (Pos p2) = length (interpolate i p1 p2) == fromIntegral i
+prop_InterpolateEndPoint (SI i) (Pos p1) (Pos p2) = last (interpolate i p1 p2) == p2
+
+prop_runProg (SI i) prog = case runProg i prog of
+    Right _ -> True
+    _       -> False
 
 return []
 runTests = $quickCheckAll
