b7addb16d1d351fc0a43c96a11391551a1d73353
Andreas Dahl <andreas@hrdahl.dk>
2015-09-22 17:17:20 +0200

    started assignment 3

---
+++ ass3/facein.pl
@@ -0,0 +1,7 @@
+facein = [person(andrzej, [susan, ken]),
+ person(ken, [andrzej]),
+ person(susan, [reed, jessica, jen, andrzej]),
+ person(reed, [tony, jessica]),
+ person(jessica, [jen]),
+ person(tony, []),
+ person(jen, [susan, jessica, tony])].
