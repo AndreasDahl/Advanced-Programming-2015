b78641146be33b0b68af7a416d32a90b197b5234
Andreas Dahl <andreas@hrdahl.dk>
2015-09-24 13:18:36 +0200

    member predicate

--- ass3/facein.pl
+++ ass3/facein.pl
@@ -1,7 +1,10 @@
-facein = [person(andrzej, [susan, ken]),
+facein([person(andrzej, [susan, ken]),
  person(ken, [andrzej]),
  person(susan, [reed, jessica, jen, andrzej]),
  person(reed, [tony, jessica]),
  person(jessica, [jen]),
  person(tony, []),
- person(jen, [susan, jessica, tony])].
+ person(jen, [susan, jessica, tony])]).
+
+member(P, [P | _]).
+member(P, [_ | Tail]) :- member(P, Tail).
