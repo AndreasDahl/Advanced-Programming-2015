9bfb2d24a2561edbe154aee58b0bc3bb774b6523
Andreas Dahl <andreas@hrdahl.dk>
2015-09-24 15:19:54 +0200

    Exercise 1 reverse

--- ass3/exercise3.pl
+++ ass3/exercise3.pl
@@ -2,3 +2,19 @@ append([], X, X).
 append([X|Y], Z, [X|W]) :- append(Y, Z, W).
 
 %reverse
+/* Bad solution
+last([Last | []], Last).
+last([_ | Tail], Last) :- last(Tail, Last).
+
+headL([_], []).
+headL([Head | Tail], [Head | Return]) :- headL(Tail, Return).
+
+reverse([], []).
+reverse([Head | Tail], Out) :- last(Out, Head), headL(Out, X), reverse(Tail, X).
+*/
+
+% Solution by the help of Oleks
+reverseHelp([], Ret, Ret).
+reverseHelp([Head | Tail], X, Y) :- reverseHelp(Tail, [Head | X], Y).
+
+reverse2(X, Y) :- reverseHelp(X, [], Y).
