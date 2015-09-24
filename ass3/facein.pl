15b0dfc167a0b18a4be627a8cd76957f97122b9c
Andreas Dahl <andreas@hrdahl.dk>
2015-09-24 13:42:52 +0200

    goodfriends

--- ass3/facein.pl
+++ ass3/facein.pl
@@ -8,3 +8,8 @@ facein([person(andrzej, [susan, ken]),
 
 member(P, [P | _]).
 member(P, [_ | Tail]) :- member(P, Tail).
+
+friend([person(P1, Friends) | _], P1, P2) :- member(P2, Friends).
+friend([_ | Tail], P1, P2) :- friend(Tail, P1, P2).
+
+goodfriends(G, P1, P2) :- friend(G, P1, P2), friend(G, P2, P1).
