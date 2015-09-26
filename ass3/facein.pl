04174b4d829d75c83c0995b1172df417b13d269a
Andreas Dahl <andreas@hrdahl.dk>
2015-09-26 15:45:17 +0200

    createChain iteration 1

--- ass3/facein.pl
+++ ass3/facein.pl
@@ -4,6 +4,9 @@ mymember(P, [_ | Tail]) :- mymember(P, Tail).
 friend([person(P1, Friends) | _], P1, P2) :- mymember(P2, Friends).
 friend([_ | Tail], P1, P2) :- friend(Tail, P1, P2).
 
+friendsOf([person(P1, Ret) | _], P1, Ret).
+friendsOf([_ | Tail], P1, Ret) :- friendsOf(Tail, P1, Ret).
+
 goodfriends(G, P1, P2) :- friend(G, P1, P2), friend(G, P2, P1).
 
 
@@ -33,4 +36,8 @@ addUniques([A | X], List, Ret) :-
     addUnique(A, List, ListAdded),
     addUniques(X, ListAdded, Ret).
 
-%createChain(G, List, person()) :- 
+createChain(_, [], _).
+createChain(G, [Head | Tail], Ret) :-
+    friendsOf(G, Head, X),
+    addUniques(X, Tail, L),
+    addUnique(Head, L, Ret).
