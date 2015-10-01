aa1f0dc3a41fe34e0c7fe94eebb6ec8af80a2567
Andreas Dahl <andreas@hrdahl.dk>
2015-10-01 13:55:40 +0200

    comment out slide code

--- ass4/facein.erl
+++ ass4/facein.erl
@@ -1,48 +1,51 @@
 -module(facein).
--export([start/0, add/2, list_all/1, update/2]).
-
-start() -> spawn(fun() -> loop(dict:new()) end).
-
-add(Pid, Contact) ->
-    blocking(Pid, {add, Contact}).
-
-list_all(Pid) ->
-    blocking(Pid, list_all).
-
-update(Pid, Contact) ->
-    blocking(Pid, {update, Contact}).
 
 
-% Internals
 
-blocking(Pid, Request) ->
-    Pid ! {self(), Request},
-    receive
-        {Pid, Response} -> Response
-    end.
-
-loop(Contacts) ->
-    receive
-        {From, {add, Contact}} ->
-            {Name,_,_} = Contact,
-            case dict:is_key(Name, Contacts) of
-                false ->
-                    From ! {self(), ok},
-                    loop(dict:store(Name, Contact, Contacts));
-                true ->
-                    From ! {self(), {error, Name, is_already_there}},
-                    loop(Contacts)
-            end;
-        {From, list_all} ->
-            List = dict:to_list(Contacts),
-            From ! {self(), {ok, lists:map(fun({_, C}) -> C end, List)}},
-            loop(Contacts);
-        {From, {update, Contact}} ->
-            {Name,_,_} = Contact,
-            NewContacts = dict:erase(Name, Contacts),
-            From ! {self(), ok},
-            loop(dict:store(Name, Contact, NewContacts));
-        {From, Other} ->
-            From ! {self(), {error,unknow_request, Other}},
-            loop(Contacts)
-    end.
+% -export([start/0, add/2, list_all/1, update/2]).
+%
+% start() -> spawn(fun() -> loop(dict:new()) end).
+%
+% add(Pid, Contact) ->
+%     blocking(Pid, {add, Contact}).
+%
+% list_all(Pid) ->
+%     blocking(Pid, list_all).
+%
+% update(Pid, Contact) ->
+%     blocking(Pid, {update, Contact}).
+%
+%
+% % Internals
+%
+% blocking(Pid, Request) ->
+%     Pid ! {self(), Request},
+%     receive
+%         {Pid, Response} -> Response
+%     end.
+%
+% loop(Contacts) ->
+%     receive
+%         {From, {add, Contact}} ->
+%             {Name,_,_} = Contact,
+%             case dict:is_key(Name, Contacts) of
+%                 false ->
+%                     From ! {self(), ok},
+%                     loop(dict:store(Name, Contact, Contacts));
+%                 true ->
+%                     From ! {self(), {error, Name, is_already_there}},
+%                     loop(Contacts)
+%             end;
+%         {From, list_all} ->
+%             List = dict:to_list(Contacts),
+%             From ! {self(), {ok, lists:map(fun({_, C}) -> C end, List)}},
+%             loop(Contacts);
+%         {From, {update, Contact}} ->
+%             {Name,_,_} = Contact,
+%             NewContacts = dict:erase(Name, Contacts),
+%             From ! {self(), ok},
+%             loop(dict:store(Name, Contact, NewContacts));
+%         {From, Other} ->
+%             From ! {self(), {error,unknow_request, Other}},
+%             loop(Contacts)
+%     end.
