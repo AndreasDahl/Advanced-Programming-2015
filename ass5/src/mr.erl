1263f51c93b0e3d7508d38dfaf50272c00b70499
Andreas Dahl <andreas@hrdahl.dk>
2015-10-10 21:35:36 +0200

    Synchronously return result

--- ass5/src/mr.erl
+++ ass5/src/mr.erl
@@ -2,7 +2,7 @@
 
 %% mr: mr library's entry point.
 
--export([start/0, job/6, stop/1]).
+-export([start/0, job/6, stop/1, test_sum/0]).
 
 
 %% API
@@ -11,12 +11,28 @@ start() ->
     gen_fsm:start({local, master}, master, [], []).
 
 job(Pid, NumWork, MapFun, RedFun, Initial, Data) ->
-    gen_fsm:send_event(Pid, {job, NumWork, MapFun, RedFun, Initial, Data}).
+    {ok, gen_fsm:sync_send_event(Pid, {job, NumWork, MapFun, RedFun, Initial, Data})}.
     % return {ok, Result} if it succeeds.
 
 stop(Pid) ->
     gen_fsm:stop(Pid).
 
+test_sum() ->
+    {ok, MR}  = mr:start(),
+    {ok, Sum} = mr:job(MR,
+                       3,
+                       fun(X) -> X end,
+                       fun(X, Acc) -> X + Acc end,
+                       0,
+                       lists:seq(1,10)),
+    {ok, Fac} = mr:job(MR,
+                       4,
+                       fun(X) -> X end,
+                       fun(X, Acc) -> X * Acc end,
+                       1,
+                       lists:seq(1,10)),
+    mr:stop(MR),
+    {Sum, Fac}.
 
 % job(Pid, NumWork, MapFun, RedFun, Initial, Data) ->
 %     end.
