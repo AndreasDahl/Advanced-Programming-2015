e6c373f8bd40995783d91840bd9aa18cad768c75
Christian Muf <chr.muf@gmail.com>
2015-10-10 14:20:34 +0200

    Dummy functions

---
+++ ass5/src/mr.erl
@@ -0,0 +1,17 @@
+-module(mr).
+-export([start/0, job/6, stop/1, advanced_job/6]).
+
+start() ->
+    end.
+
+job(Pid, NumWork, MapFun, RedFun, Initial, Data) ->
+    end.
+
+stop(Pid) ->
+    end.
+
+advanced_job(Pid, NumWork, MapFun, RedFun, Initial, Data) ->
+    end.
+
+% Private functions
+
